d21f1bb556a6abd87c1157f3a0164cb2991d487b
Amon Eliel <amonpaterne7@gmail.com>
2026-05-24 10:26:06 +0000

    Code modifié

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,47 @@
 public class Main {
     public static void main(String[] args) {
-        // Création des comptes
-        CheckingAccount compteCourant = new CheckingAccount("CC-001", 1000.0, 500.0);
-        SavingsAccount compteEpargne = new SavingsAccount("CE-001", 2000.0, 2.5);
-        COD certificatDepot = new COD("COD-001", 5000.0, 12);
 
-        // Lecture et modification des attributs
-        System.out.println("Compte courant : " + compteCourant.getCompte() + " - solde : " + compteCourant.getSolde());
-        compteCourant.setSolde(compteCourant.getSolde() + 250.0);
+        // Création des 3 comptes
+        CheckingAccount compteCourant   = new CheckingAccount("CC-001", 650000.0, 325000.0);
+        SavingsAccount  compteEpargne   = new SavingsAccount("CE-001", 1300000.0, 2.5);
+        COD             certificatDepot = new COD("COD-001", 3250000.0, 12);
 
-        System.out.println("Compte épargne : " + compteEpargne.getCompte() + " - solde : " + compteEpargne.getSolde());
+
+        // Compte Courant
+        System.out.println(" Compte Courant ");
+        System.out.println("Numéro  : " + compteCourant.getCompte());
+        System.out.println("Solde   : " + compteCourant.getSolde() + " FCFA");
+        System.out.println("Limite  : " + compteCourant.getLimite() + " FCFA");
+
+        compteCourant.setSolde(compteCourant.getSolde() + 163000.0);
+        System.out.println("Après dépôt de 163 000 FCFA → Solde : " + compteCourant.getSolde() + " FCFA");
+
+
+        // Compte Épargne
+        System.out.println("\n Compte Épargne ");
+        System.out.println("Numéro  : " + compteEpargne.getCompte());
+        System.out.println("Solde   : " + compteEpargne.getSolde() + " FCFA");
+        System.out.println("Taux    : " + compteEpargne.getTauxInteret() + " %");
+
         compteEpargne.appliquerInteret();
+        System.out.println("Après intérêts → Solde : " + compteEpargne.getSolde() + " FCFA");
 
-        System.out.println("Certificat de dépôt : " + certificatDepot.getCompte() + " - solde : " + certificatDepot.getSolde());
+
+        // Certificat de Dépôt
+        System.out.println("\n Certificat de Dépôt ");
+        System.out.println("Numéro  : " + certificatDepot.getCompte());
+        System.out.println("Solde   : " + certificatDepot.getSolde() + " FCFA");
+        System.out.println("Durée   : " + certificatDepot.getDureeMois() + " mois");
+
+
+        // Modification et relecture (prouve get + set)
+        System.out.println("\n Modification des comptes ");
+
+        compteCourant.setCompte("CC-001-MAJ");
+        System.out.println("Nouveau numéro compte courant : " + compteCourant.getCompte());
+
+        compteEpargne.setSolde(1950000.0);
+        System.out.println("Nouveau solde épargne : " + compteEpargne.getSolde() + " FCFA");
+
     }
 }
